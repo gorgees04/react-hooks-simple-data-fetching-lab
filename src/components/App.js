535e7a68c39d58bc829d53a2f96bc5b8b11b1f38
Gorgees Odisho <gorgees041@gmail.com>
2023-10-28 03:21:31 +0300

    FETCHING ASSIGNMENT DONE

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [breedsData, setBreedsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((data) => {
+        setBreedsData(data);
+        setLoading(false);
+      });
+  }, []);
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={breedsData.message} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
